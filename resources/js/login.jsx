951ff5e0c074736f5d0285ef636cd5f3098b8cfc
misteina <enyinna.mails@gmail.com>
2021-11-07 20:06:37 +0100

    Frontend

---
+++ resources/js/login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Login(){
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [showError, setShowError] = useState(false);
+
+    const fillUserName = (e) => setUserName(e.target.value);
+    const fillPassword = (e) => setPassword(e.target.value);
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        let csrfToken = document.querySelector('meta[name="csrf-token"]')['content'];
+
+        fetch('/login', {
+            method: 'post',
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: `username=${userName}&password=${password}&_token=${csrfToken}`
+        }).then(
+            response => response.json()
+        ).then(
+            data => {
+                if (data.message === 'success'){
+                    location.href = '/todos'
+                } else {
+                    setShowError(true);
+                }
+            }
+        ).catch(function (error) {
+            console.log('Request failed', error);
+        });
+    }
+
+    return (
+        <div id="form">
+            <span>Login</span>
+            <form onSubmit={submitForm}>
+                <Error display={showError} />
+                <input type="text" maxLength="30" minLength="3" placeholder="User name" onChange={fillUserName} value={userName} />
+                <input type="text" maxLength="20" minLength="3" placeholder="Password" onChange={fillPassword} value={password} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+function Error({display}){
+    if (display){
+        return <div id="error">Incorrect email or password</div>
+    }
+    return null;
+}
+
+ReactDOM.render(<Login />, document.getElementById('content'));
---
+++ resources/js/register.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Register(){
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [showError, setShowError] = useState(false);
+
+    const fillUserName = (e) => setUserName(e.target.value);
+    const fillPassword = (e) => setPassword(e.target.value);
+    const fillConfirmPassword = (e) => setConfirmPassword(e.target.value);
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        let csrfToken = document.querySelector('meta[name="csrf-token"]')['content'];
+
+        fetch('/register', {
+            method: 'post',
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: `username=${userName}&password=${password}&password_confirmation=${confirmPassword}&_token=${csrfToken}`
+        }).then(
+            response => response.json()
+        ).then(
+            data => {
+                if (data.message === 'success'){
+                    location.href = '/login'
+                } else {
+                    setShowError(true);
+                }
+            }
+        ).catch(function (error) {
+            console.log('Request failed', error);
+        });
+    }
+
+    return (
+        <div id="form">
+            <span>Register</span>
+            <form onSubmit={submitForm}>
+                <Error display={showError} />
+                <input type="text" maxLength="30" minLength="3" placeholder="User name" onChange={fillUserName} value={userName} />
+                <input type="text" maxLength="20" minLength="3" placeholder="Password" onChange={fillPassword} value={password} />
+                <input type="text" maxLength="20" minLength="5" placeholder="Confirm Password" onChange={fillConfirmPassword} value={confirmPassword} />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+function Error({display}){
+    if (display){
+        return <div id="error">Registration failed</div>
+    }
+    return null;
+}
+
+ReactDOM.render(<Register />, document.getElementById('content'));
