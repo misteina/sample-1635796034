c8e6ac4c8826e23f6ca28193856b0d266d8aae61
misteina <enyinna.mails@gmail.com>
2021-11-08 22:26:04 +0100

    Profile controller

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $name = User::find(Auth::id())->name;
+
+        return view('profile', ['name' => $name]);
+    }
+}
