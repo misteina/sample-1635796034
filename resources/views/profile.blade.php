c0992f126fb6fc151ce95132e1c796cec16cfdc7
misteina <enyinna.mails@gmail.com>
2021-11-09 22:11:11 +0100

    Profile view

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot name="title">Profile</x-slot>
+    <div id="profile">
+        <span>Welcome, <b>{{ ucwords($name) }}</b></span>
+    </div>
+    <x-slot name="script">
+        <script src="{{ mix('js/profile.js') }}"></script>
+    </x-slot>
+</x-layout>
