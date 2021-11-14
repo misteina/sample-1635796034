39246f12df8b63ba20b5f8ab5959dd05f20e1bf9
misteina <enyinna.mails@gmail.com>
2021-11-15 00:14:27 +0100

    Refactor

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">Profile</x-slot>
     <div id="profile">
         <span>Welcome, <b>{{ ucwords($name) }}</b></span>
-        <span><a href="/logout">Logout</a></span>
+        <div><a href="/logout">Logout</a></div>
     </div>
     <x-slot name="script">
         <script src="{{ mix('js/profile.js') }}"></script>
